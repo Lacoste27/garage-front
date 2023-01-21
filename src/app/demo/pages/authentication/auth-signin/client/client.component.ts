bd438dc14238f27344c47e6150523f195f570b93
lacoste27 <robsonatsiory07@gmail.com>
2023-01-21 19:29:32 +0300

    [FIX] Signin

--- src/app/demo/pages/authentication/auth-signin/client/client.component.ts
+++ src/app/demo/pages/authentication/auth-signin/client/client.component.ts
@@ -1,51 +1,91 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router, RouterModule } from '@angular/router';
-import { IResponse } from 'src/app/demo/interfaces/interface';
-import { AuthentificationService } from 'src/app/demo/services/authentification/authentification.service';
-import { TokenService } from 'src/app/demo/services/jwt/token.service';
-import { NavigationItem } from 'src/app/theme/layout/admin/navigation/navigation';
+import { CommonModule } from "@angular/common";
+import { Component, OnInit } from "@angular/core";
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from "@angular/forms";
+import { Router, RouterModule } from "@angular/router";
+import { NgbDropdownModule } from "@ng-bootstrap/ng-bootstrap";
+import { ColorPickerModule } from "ngx-color-picker";
+import { IResponse } from "src/app/demo/interfaces/interface";
+import { AuthentificationService } from "src/app/demo/services/authentification/authentification.service";
+import { TokenService } from "src/app/demo/services/jwt/token.service";
+import { NavigationItem } from "src/app/theme/layout/admin/navigation/navigation";
+import { SharedModule } from "src/app/theme/shared/shared.module";
 
 @Component({
   selector: "app-client",
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [
+    CommonModule,
+    SharedModule,
+    NgbDropdownModule,
+    ColorPickerModule,
+    RouterModule,
+  ],
   templateUrl: "./client.component.html",
   styleUrls: ["./client.component.scss"],
 })
-export class ClientComponent {
+export class ClientComponent implements OnInit {
   loading: boolean = false;
   erreur: boolean = false;
-  form: FormGroup;
-  private login: string = "yroist@email.com";
-  private password: string = "passwords";
+  submitted: boolean = false;
+  form: FormGroup = new FormGroup({
+    email: new FormControl(""),
+    password: new FormControl(""),
+  });
 
   constructor(
     private authService: AuthentificationService,
-    private router: Router,
     private formBuilder: FormBuilder,
+    private router: Router,
     private token: TokenService,
     private navigation: NavigationItem
-  ) {
+  ) {}
+
+  ngOnInit(): void {
     this.form = this.formBuilder.group({
       email: ["", Validators.required],
       password: ["", Validators.required],
     });
   }
 
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
+
   signin() {
     const value = this.form.value;
     this.loading = true;
+    this.submitted = true;
 
-    this.authService.login(this.login, this.password,'client').subscribe((result) => {
-      const response: IResponse = result as IResponse;
+    if (this.form.invalid) {
       this.loading = false;
-      if (response.success) {
-        this.token.SetToken(response.data.token);
-        this.navigation.set();
-        this.router.navigateByUrl("/dashboard");
-      }
+      return;
+    }
+
+    this.authService.login(value.email, value.password, "client").subscribe({
+      next: (result) => {
+        const response: IResponse = result as IResponse;
+        this.loading = false;
+        if (response.success) {
+          this.token.SetToken(response.data.token);
+          this.navigation.set();
+          this.router.navigateByUrl("/dashboard");
+        } else {
+          this.erreur = true;
+          this.loading = false;
+          return;
+        }
+      },
+      error: (msg) => {
+        this.erreur = true;
+        this.loading = false;
+        return;
+      },
     });
   }
 }
--- src/app/demo/pages/authentication/auth-signin/responsable/responsable.component.ts
+++ src/app/demo/pages/authentication/auth-signin/responsable/responsable.component.ts
@@ -1,25 +1,35 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router, RouterModule } from '@angular/router';
-import { IResponse } from 'src/app/demo/interfaces/interface';
-import { AuthentificationService } from 'src/app/demo/services/authentification/authentification.service';
-import { TokenService } from 'src/app/demo/services/jwt/token.service';
-import { NavigationItem } from 'src/app/theme/layout/admin/navigation/navigation';
+import { CommonModule } from "@angular/common";
+import { Component, OnInit } from "@angular/core";
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from "@angular/forms";
+import { Router, RouterModule } from "@angular/router";
+import { IResponse } from "src/app/demo/interfaces/interface";
+import { AuthentificationService } from "src/app/demo/services/authentification/authentification.service";
+import { TokenService } from "src/app/demo/services/jwt/token.service";
+import { NavigationItem } from "src/app/theme/layout/admin/navigation/navigation";
+import { SharedModule } from "src/app/theme/shared/shared.module";
 
 @Component({
-  selector: 'app-responsable',
+  selector: "app-responsable",
   standalone: true,
-  imports: [CommonModule, RouterModule],
-  templateUrl: './responsable.component.html',
-  styleUrls: ['./responsable.component.scss']
+  imports: [CommonModule, RouterModule,SharedModule],
+  templateUrl: "./responsable.component.html",
+  styleUrls: ["./responsable.component.scss"],
 })
-export class ResponsableComponent {
+export class ResponsableComponent implements OnInit {
   loading: boolean = false;
   erreur: boolean = false;
-  form: FormGroup;
-  private login: string = "atelier@email.com";
-  private password: string = "passwords";
+  submitted: boolean = true;
+
+  form: FormGroup = new FormGroup({
+    email: new FormControl(""),
+    password: new FormControl(""),
+  });
 
   constructor(
     private authService: AuthentificationService,
@@ -27,27 +37,51 @@ export class ResponsableComponent {
     private formBuilder: FormBuilder,
     private token: TokenService,
     private navigation: NavigationItem
-  ) {
+  ) {}
+
+  ngOnInit(): void {
     this.form = this.formBuilder.group({
       email: ["", Validators.required],
       password: ["", Validators.required],
     });
   }
 
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
+
   signin() {
     const value = this.form.value;
     this.loading = true;
+    this.submitted = true;
 
-    this.authService.login(this.login, this.password, 'responsable').subscribe((result) => {
-      const response: IResponse = result as IResponse;
-      console.log(response);
-
+    if (this.form.invalid) {
       this.loading = false;
-      if (response.success) {
-        this.token.SetToken(response.data.token);
-        this.navigation.set();
-        this.router.navigateByUrl("/dashboard");
-      }
-    });
+      return;
+    }
+
+    this.authService
+      .login(value.email, value.password, "responsable")
+      .subscribe({
+        next: (result) => {
+          const response: IResponse = result as IResponse;
+          console.log(response);
+          this.loading = false;
+          if (response.success) {
+            this.token.SetToken(response.data.token);
+            this.navigation.set();
+            this.router.navigateByUrl("/dashboard");
+          } else {
+            this.erreur = true;
+            this.loading = false;
+            return;
+          }
+        },
+        error: (msg) => {
+          this.erreur = true;
+          this.loading = false;
+          return;
+        },
+      });
   }
 }
