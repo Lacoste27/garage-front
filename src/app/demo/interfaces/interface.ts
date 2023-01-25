cfaf1c1720d6345a928e211d9514d340de7f49a3
TommyAXIAN <zafi.tommy@gmail.com>
2023-01-25 22:11:30 +0300

    Payée

--- src/app/demo/interfaces/interface.ts
+++ src/app/demo/interfaces/interface.ts
@@ -67,3 +67,18 @@ export enum ReparationVoitureEtat {
   encours = "En cours",
   fini = "Finis",
 }
+
+
+export interface IPaiment {
+  date: string,
+  mode: string,
+  recu: string,
+  rendu: string,
+  valideur: {},
+  valid: number
+}
+
+export interface Paiement {
+  mode: string,
+  recu: string
+}
--- src/app/demo/pages/users/reparations/facture/facture/facture.component.ts
+++ src/app/demo/pages/users/reparations/facture/facture/facture.component.ts
@@ -5,6 +5,16 @@ import { IReparation, IResponse } from 'src/app/demo/interfaces/interface';
 import { ReparationService } from 'src/app/demo/services/reparations/reparation.service';
 import { CardModule } from 'src/app/theme/shared/components/card/card.module';
 import { SharedModule } from 'src/app/theme/shared/shared.module';
+import { ModalDismissReasons, NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import {
+  CdkDragDrop,
+  CdkDropList,
+  DragDropModule,
+  moveItemInArray,
+  transferArrayItem,
+} from "@angular/cdk/drag-drop";
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { UsersService } from 'src/app/demo/services/users/users.service';
 
 @Component({
   selector: 'app-facture',
@@ -18,13 +28,25 @@ export class FactureComponent implements OnInit {
   reparation: IReparation;
   montantTotal: number = 0;
   loading: boolean = false;
+  submitted: boolean = false;
+  error: boolean = false;
+  form: FormGroup = new FormGroup({
+    mode: new FormControl(""),
+    montant: new FormControl(0),
+  });
 
-  constructor(
+  constructor(private formBuilder: FormBuilder,
+    private modalService: NgbModal,
     private activatedRoute: ActivatedRoute,
-    private reparationService: ReparationService
+    private reparationService: ReparationService,
+    private userService: UsersService
   ) { }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      mode: ['', Validators.required],
+      montant: [0, Validators.required]
+    })
     this.loading = true;
     this.activatedRoute.params.subscribe((params) => {
       this.id = params["id"];
@@ -40,5 +62,47 @@ export class FactureComponent implements OnInit {
           }
         });
     });
+  }
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
+
+  open(content: any) {
+    this.modalService
+      .open(content, {
+        ariaLabelledBy: "modal-basic-title",
+        fullscreen: false,
+        centered: true,
+      })
+  }
+
+  payerReparation() {
+    this.loading = true;
+    this.submitted = true;
+    if (this.form.invalid) {
+      this.loading = false;
+      return;
+    }
+
+    const paiement = {
+      mode: this.form.value.mode,
+      recu: this.form.value.montant,
+      reparation_id: this.id,
+    };
+
+    this.userService.payerReparation(paiement, this.reparation._id).subscribe((reponse: IResponse) => {
+      if (reponse.success) {
+        this.loading = false;
+      } else {
+        this.error = true;
+        this.loading = false;
+      }
+    })
+  }
+
+  onReset() {
+    this.submitted = false;
+    this.form.reset();
   }
 }
